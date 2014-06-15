fc6b65785932e171b5426ced64f83b65a3cbd407
sixtyeight <christian_sl@gmx.at>
2014-06-15 02:11:45 +0200

    variable substitution

--- soup.sh
+++ soup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath ./target/soup-cli-jar-0.0.1-SNAPSHOT-jar-with-dependencies.jar at.metalab.m68k.soupcli.SoupMain $*
+java -classpath ./target/soup-cli-jar-0.0.1-SNAPSHOT-jar-with-dependencies.jar at.metalab.m68k.soupcli.SoupMain "$@"
